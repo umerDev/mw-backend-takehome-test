6a7d57ebc211f7400bcf1b818fb1f2f80fb9eb6b
umerDev <umerraja@live.co.uk>
2025-04-28 18:08:06 +0100

    feat: create premiumCarValuation response object.

---
+++ src/premium-car/types/premium-car-response.ts
@@ -0,0 +1,9 @@
+export type PremiumCarValuationResponse = {
+  registrationDate: string;
+  registrationYear: number;
+  registrationMonth: number;
+  valuationPrivateSaleMinimum: number;
+  valuationPrivateSaleMaximum: number;
+  valuationDealershipMinimum: number;
+  valuationDealershipMaximum: number;
+};
